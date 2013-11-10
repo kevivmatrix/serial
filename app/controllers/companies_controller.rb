f5423885b933cee5c9db4be4ab71af990c1fab40
kevivmatrix <vivekpatna52@gmail.com>
2013-11-10 11:42:30 +0530

    controller with json response

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,20 @@
 class CompaniesController < ApplicationController
   def index
+    @companies = Company.all
+    respond_to do |f|
+      f.html
+      f.json { render json: @companies }
+    end
   end
 
   def show
+    @company = Company.find(params[:id])
+    respond_to do |f|
+      f.html
+      f.json { 
+        render json: @company
+      }
+    end
   end
+
 end
