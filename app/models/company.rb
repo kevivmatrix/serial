ee598c21dca04852a7dc53e19933b185475570bf
kevivmatrix <vivekpatna52@gmail.com>
2013-11-10 10:48:10 +0530

    model associations fix

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ActiveRecord::Base
 
   has_many :employees
+  has_many :offices
 
 end
--- app/models/office.rb
+++ app/models/office.rb
@@ -1,5 +1,6 @@
 class Office < ActiveRecord::Base
 
   belongs_to :company
+  has_many :employees
 
 end
