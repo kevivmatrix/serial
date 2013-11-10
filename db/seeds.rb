bece1d17ce24236d69f362e61a0d54196c7f5fd8
kevivmatrix <vivekpatna52@gmail.com>
2013-11-10 10:40:11 +0530

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# companies
+Company.destroy_all;
+c1 = Company.create name: "CodeBrahma", founded: (Date.today - 1.year)
+
+# office
+Office.destroy_all;
+o1 = Office.create location: "Bangalore", company_id: c1.id
+o2 = Office.create location: "Chennai", company_id: c1.id
+
+# employees
+Employee.destroy_all;
+Employee.create name: "A", designation: "web developer", month_salary: 40000, company_id: c1.id, office_id: o1.id
+Employee.create name: "B", designation: "manager", month_salary: 60000, company_id: c1.id, office_id: o1.id
+Employee.create name: "C", designation: "secretary", month_salary: 20000, company_id: c1.id, office_id: o2.id
+Employee.create name: "D", designation: "receptionist", month_salary: 40000, company_id: c1.id, office_id: o2.id
+Employee.create name: "E", designation: "founder", month_salary: 100000, company_id: c1.id, office_id: o2.id
